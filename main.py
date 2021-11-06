600d971d56c4ac7532fe1e9b899eb67dc029be7f
adn6868 <44442570+adn6868@users.noreply.github.com>
2021-11-06 10:55:38 +0700

    add some sample api call and mock db

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import math
 from typing import List, OrderedDict
@@ -9,17 +9,19 @@ app = FastAPI()
 db = {}
 
 class Order:
-    def __init__ (self, order_id, items =[], customer = "unknow customer"){
+    def __init__ (self, order_id, items_list =[], customer = "unknow customer"):
         self.order_id = order_id
-        self.items = []
+        self.items_list = []
         self.customer = customer
-    }
+        
     def get_order_id(self):
         return self.order_id
     def add_item(self, item):
-        self.items.append(item)
+        self.items_list.append(item)
     def set_customer(self,customer):
         self.customer = customer
+    def __str__(self):
+        return self.__dict__
     
 class Item:
     def __init__ (self, item_id, name ):
@@ -28,10 +30,38 @@ class Item:
 
 class _Order (BaseModel):
     order_id : int
-    items : List[int]
+    items_list : List[int]
     customer : str
 
 class _Item (BaseModel):
     item_id : int
     name : str
 
+@app.get('/')
+def index():
+	return {'msg','Api Alive'}
+
+@app.post('/place_order')
+def place_order(new_order: _Order)->str:
+    order_id = new_order.order_id
+    if order_id in db.keys():
+        raise HTTPException(status_code=409, detail="Conflict, order_id already exist in database")
+    db[order_id] = new_order
+
+
+@app.get('/get_order')
+def get_order(order_id: _Order.order_id)->dict:
+    if order_id not in db.keys():
+        raise HTTPException(status_code=404, detail="Cannot find order in DB")
+    order = db[order_id]
+    return order.__dict__
+    
+@app.get('/DB')
+def getDB():
+    '''
+
+    '''
+    return [db[key] for key in db.keys()]
+
+if __name__ == '__main__':
+	uvicorn.run(app, host="127.0.0.1", port=8000)
